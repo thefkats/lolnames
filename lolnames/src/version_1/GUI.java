1ea2d9df8ad13a7d53c3d8d042e6a409147de05d
James-Beetham <lmpeters11@hotmail.com>
2017-04-06 15:13:46 -0500

    Added GUI

---
+++ lolnames/src/version_1/GUI.java
@@ -0,0 +1,53 @@
+package version_1;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI
+{
+    public static void main(String[] args) throws IOException, URISyntaxException
+    {
+
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+	frame.setSize(500, 500);
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	JPanel pan = new JPanel(new FlowLayout());
+	JButton runButton = new JButton("Run");
+	runButton.addActionListener(new run());
+	pan.add(runButton);
+	JButton importButton = new JButton("Import");
+	importButton.addActionListener(new imports());
+	pan.add(importButton, FlowLayout.LEFT);
+	frame.getContentPane().add(pan);
+    }
+
+    static class run implements ActionListener
+    {
+	public void actionPerformed(ActionEvent e)
+	{
+	    System.out.println("run");
+	}
+    }
+
+    static class imports implements ActionListener
+    {
+	public void actionPerformed(ActionEvent e)
+	{
+	    JFrame frame2 = new JFrame();
+	    frame2.setDefaultCloseOperation(frame2.EXIT_ON_CLOSE);
+	    JFileChooser fc = new JFileChooser();
+	    File f = fc.getSelectedFile();
+	    System.out.println(f.getName());
+	}
+    }
+
+}
